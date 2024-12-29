d995a365dd9d392103811debb6ab120dd641b72d
read0more <read0more@gmail.com>
2024-12-29 17:18:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Create a Design System with CSS - Web Development Course
+
+https://www.youtube.com/watch?v=lRaL-8qZ0mM
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from './App.module.scss'
+
+function App() {
+
+  return (
+    <div className={`container grid ${styles.boxes}`}>
+      <div className={`bg-accent text-dark ${styles.box}`}>a</div>
+      <div className={`bg-white text-dark ${styles.box}`}>b</div>
+      <div className={`bg-accent-alpha text-white ${styles.box}`}>c</div>
+    </div>
+  )
+}
+
+export default App
